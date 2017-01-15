d49206856c59f45cda716b93dd5f52152b7a37ee
Edward Meng <edwardmeng@live.cn>
2017-01-15 16:06:14 +0800

    Add unit tests for the FilteredUserClaimsPrincipalFactory.

---
+++ Lenoard.AspNetCore.Testing.UnitTests/InMemoryRoleStore.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace Lenoard.AspNetCore.UnitTests
+{
+    public class InMemoryRoleStore<TRole>: 
+        IQueryableRoleStore<TRole>,
+        IRoleClaimStore<TRole>
+        where TRole: TestRole
+    {
+
+        private readonly Dictionary<string, TRole> _roles = new Dictionary<string, TRole>();
+
+        public Task<IdentityResult> CreateAsync(TRole role, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            _roles[role.Id] = role;
+            return Task.FromResult(IdentityResult.Success);
+        }
+
+        public Task<IdentityResult> DeleteAsync(TRole role, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            if (role == null || !_roles.ContainsKey(role.Id))
+            {
+                throw new InvalidOperationException("Unknown role");
+            }
+            _roles.Remove(role.Id);
+            return Task.FromResult(IdentityResult.Success);
+        }
+
+        public Task<string> GetRoleIdAsync(TRole role, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return Task.FromResult(role.Id);
+        }
+
+        public Task<string> GetRoleNameAsync(TRole role, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return Task.FromResult(role.Name);
+        }
+
+        public Task SetRoleNameAsync(TRole role, string roleName, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            role.Name = roleName;
+            return Task.FromResult(0);
+        }
+
+        public Task<IdentityResult> UpdateAsync(TRole role, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            _roles[role.Id] = role;
+            return Task.FromResult(IdentityResult.Success);
+        }
+
+        Task<TRole> IRoleStore<TRole>.FindByIdAsync(string roleId, CancellationToken cancellationToken)
+        {
+            if (_roles.ContainsKey(roleId))
+            {
+                return Task.FromResult(_roles[roleId]);
+            }
+            return Task.FromResult<TRole>(null);
+        }
+
+        Task<TRole> IRoleStore<TRole>.FindByNameAsync(string roleName, CancellationToken cancellationToken)
+        {
+            return
+                Task.FromResult(
+                    Roles.SingleOrDefault(r => String.Equals(r.NormalizedName, roleName, StringComparison.OrdinalIgnoreCase)));
+        }
+
+        public Task<IList<Claim>> GetClaimsAsync(TRole role, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            var claims = role.Claims.Select(c => new Claim(c.ClaimType, c.ClaimValue)).ToList();
+            return Task.FromResult<IList<Claim>>(claims);
+        }
+
+        public Task AddClaimAsync(TRole role, Claim claim, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            role.Claims.Add(new TestRoleClaim<string> { ClaimType = claim.Type, ClaimValue = claim.Value, RoleId = role.Id });
+            return Task.FromResult(0);
+        }
+
+        public Task RemoveClaimAsync(TRole role, Claim claim, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            var entity =
+                role.Claims.FirstOrDefault(
+                    ur => ur.RoleId == role.Id && ur.ClaimType == claim.Type && ur.ClaimValue == claim.Value);
+            if (entity != null)
+            {
+                role.Claims.Remove(entity);
+            }
+            return Task.FromResult(0);
+        }
+
+        public Task<string> GetNormalizedRoleNameAsync(TRole role, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return Task.FromResult(role.NormalizedName);
+        }
+
+        public Task SetNormalizedRoleNameAsync(TRole role, string normalizedName, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            role.NormalizedName = normalizedName;
+            return Task.FromResult(0);
+        }
+
+        public IQueryable<TRole> Roles => _roles.Values.AsQueryable();
+
+        public void Dispose()
+        {
+        }
+    }
+}
---
+++ Lenoard.AspNetCore.Testing.UnitTests/TestUserClaimsPrincipalFilter.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Lenoard.AspNetCore.UnitTests
+{
+    public class TestUserClaimsPrincipalFilter: IUserClaimsPrincipalFilter<TestUser>
+    {
+        public Task ConfigureAsync(TestUser user, ClaimsPrincipal principal)
+        {
+            ((ClaimsIdentity)principal.Identity).AddClaim(new Claim("Permission", "TestUserPermission"));
+            return Task.Delay(0);
+        }
+    }
+}
